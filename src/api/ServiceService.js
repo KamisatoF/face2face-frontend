99f96efd8b3112c46e9ac673be4ad3aa79f7410a
Fabio Kamisato <fabiokamisato@gmail.com>
2022-08-08 17:30:03 -0300

    Ajuste URL

--- src/api/ServiceService.js
+++ src/api/ServiceService.js
@@ -1,22 +1,22 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://puc-face2face.herokuapp.com/';
+const BASE_URL = 'http://puc-face2face.herokuapp.com/servicos/';
 const withBaseUrl = path => '${BASE_URL}${path}';
 
 export class ServiceService {
     static findAll() {
-        return axios('https://puc-face2face.herokuapp.com/servicos/findAll/')
+        return axios('http://puc-face2face.herokuapp.com/servicos/findAll/')
     }
 
     static insert(service) {
-        return axios.post('https://puc-face2face.herokuapp.com/', service).then(this.findAll)
+        return axios.post('http://puc-face2face.herokuapp.com/servicos/', service).then(this.findAll)
     }
 
     static delete(id) {
-        return axios.delete('https://puc-face2face.herokuapp.com/' + id).then(this.findAll)
+        return axios.delete('http://puc-face2face.herokuapp.com/servicos/' + id).then(this.findAll)
     }
 
     static update(service) {
-        return axios.put('https://puc-face2face.herokuapp.com/' + service.id, service, service.id).then(this.findAll)
+        return axios.put('http://puc-face2face.herokuapp.com/servicos/' + service.id, service, service.id).then(this.findAll)
     }
 }
