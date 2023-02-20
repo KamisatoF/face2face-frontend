f055ba5065561500429317f64eecaed45a14ac78
Fabio Kamisato <fabiokamisato@gmail.com>
2023-02-20 10:45:46 -0300

    Migrando pra azure

--- src/api/ServiceService.js
+++ src/api/ServiceService.js
@@ -1,22 +1,22 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://puc-face2face.herokuapp.com/servicos/';
+const BASE_URL = 'https://face2face-backend.azurewebsites.net/servicos/';
 const withBaseUrl = path => '${BASE_URL}${path}';
 
 export class ServiceService {
     static findAll() {
-        return axios('https://puc-face2face.herokuapp.com/servicos/findAll/')
+        return axios('https://face2face-backend.azurewebsites.net/servicos/findAll/')
     }
 
     static insert(service) {
-        return axios.post('https://puc-face2face.herokuapp.com/servicos/', service).then(this.findAll)
+        return axios.post('https://face2face-backend.azurewebsites.net/servicos/', service).then(this.findAll)
     }
 
     static delete(id) {
-        return axios.delete('https://puc-face2face.herokuapp.com/servicos/' + id).then(this.findAll)
+        return axios.delete('https://face2face-backend.azurewebsites.net/servicos/' + id).then(this.findAll)
     }
 
     static update(service) {
-        return axios.put('https://puc-face2face.herokuapp.com/servicos/' + service.id, service, service.id).then(this.findAll)
+        return axios.put('https://face2face-backend.azurewebsites.net/servicos/' + service.id, service, service.id).then(this.findAll)
     }
 }
